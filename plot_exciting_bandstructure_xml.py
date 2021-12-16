98f4e8e48fc8bdc571fbb751ced64865e271227f
Giovani L. Rech <gio.pi.rech@gmail.com>
2021-12-16 09:13:11 -0300

    Add atomatic detection of number of orbitals and the option --Eshift

--- plot_exciting_bandstructure_xml.py
+++ plot_exciting_bandstructure_xml.py
@@ -13,39 +13,49 @@ def read_bands_Ldiff(xml_file):
     #Vamos criar um dicionário com as bandas separadas por elemento (species)
     
     dictionary_species = {}
-    for species_i in range(len(bandstrucutre.findall('species'))):
+    N_species = len(bandstrucutre.findall('species'))
+    if args.verbose:
+        print(f'Found {N_species} species in the file:',*[bandstrucutre.findall('species')[species_i].attrib['chemicalSymbol'] for species_i in range(N_species)])
+        
+    for species_i in range(N_species):
         
         key_element_symbol = bandstrucutre.findall('species')[species_i].attrib['chemicalSymbol']
-    
-        bands = bandstrucutre.findall('species')[species_i].getchildren()[0].getchildren() #todas as bandas do atomo
-        #       ^root         ^species                      ^atom            ^bands
+        
+        N_atoms = len(bandstrucutre.findall('species')[species_i].getchildren())
         
         
         
-        #lista de dicionarios. Cada dicionario é uma banda
-        lista_bandas = []
-        for band in bands:
-            distance = np.array([float(i.get('distance')) for i in band.getchildren()])
-            energy = np.array([float(i.get('eval')) for i in band.getchildren()])
-            suml = np.array([float(i.get('sum')) for i in band.getchildren()])
-            inters = np.array([1-float(i.get('sum')) for i in band.getchildren()])
-            s = np.array([float(i.getchildren()[0].get('character')) for i in band.getchildren()])
-            p = np.array([float(i.getchildren()[1].get('character')) for i in band.getchildren()])
-            d = np.array([float(i.getchildren()[2].get('character')) for i in band.getchildren()])
-            f = np.array([float(i.getchildren()[3].get('character')) for i in band.getchildren()])
-            g = np.array([float(i.getchildren()[4].get('character')) for i in band.getchildren()])
-            banda = {'distance': np.array(distance),
-                     'energy'  : np.array(energy),
-                     'sum'     : np.array(suml),
-                     'inters'  : np.array(inters),
-                     's'       : np.array(s),
-                     'p'       : np.array(p),
-                     'd'       : np.array(d),
-                     'f'       : np.array(f),
-                     'g'       : np.array(g)}
-            lista_bandas.append(banda)
+        for atom_i in range(N_atoms):
         
-        dictionary_species[key_element_symbol] = lista_bandas
+            bands = bandstrucutre.findall('species')[species_i].getchildren()[atom_i].getchildren() #todas as bandas do atomo
+            #       ^root         ^species                      ^atom            ^bands
+                       
+            #lista de dicionarios. Cada dicionario é uma banda
+            lista_bandas = []
+            for band in bands:
+                distance = np.array([float(i.get('distance')) for i in band.getchildren()])
+                energy = np.array([float(i.get('eval'))+args.Eshift for i in band.getchildren()])
+                suml = np.array([float(i.get('sum')) for i in band.getchildren()])
+                inters = np.array([1-float(i.get('sum')) for i in band.getchildren()])
+                banda = {'distance': np.array(distance),
+                         'energy'  : np.array(energy),
+                         'sum'     : np.array(suml),
+                         'inters'  : np.array(inters)}
+                
+                #now lets get the bandstructures by orbital
+                Norb = len(band.getchildren()[0].getchildren()) #number of orbitals present in the file
+                orb_keys = ['s', 'p', 'd', 'f','g', 'h']
+                for norb in range(Norb):
+                    band_orbital = np.array([float(i.getchildren()[norb].get('character')) for i in band.getchildren()])
+                    banda[orb_keys[norb]] = np.array(band_orbital)
+                lista_bandas.append(banda)
+            
+            dictionary_species[key_element_symbol+str(atom_i+1)] = lista_bandas
+            if args.verbose:
+                print(f'Specie {key_element_symbol} has {N_atoms} atoms.')
+        
+    print(f'Number of bands: {len(bands)}')
+    print(f'Number of orbitals: {Norb} {orb_keys[:Norb]}')
     ##############################################
     # Outros elementos gráficos
     vertice_labels = []
@@ -84,7 +94,7 @@ def plot_bandstructure_atoms(dictionary_species, vertice_coords, vertice_labels,
     
         #get x limits from data
         xmin,xmax = lista_bandas[0]['distance'][0],lista_bandas[0]['distance'][-1]
-    
+        Norbs = len(dictionary_species[element][0])-4 #how many orbitals in each band
         ##### Diferenciando L
         #draw curves
         if l_resolved:
@@ -92,16 +102,16 @@ def plot_bandstructure_atoms(dictionary_species, vertice_coords, vertice_labels,
                 alpha_geral = orbalpha
                 multiplier = orbthickness #define a espessura da banda de acordo com o orbital
                 ax0.plot(banda['distance'], banda['energy'], color='grey', lw=linethickness, alpha=linealpha)
-                orbitals = ['s',  'p',  'd',  'f',  'g']
+                orbitals = ['s',  'p',  'd',  'f',  'g', 'h']
                 colors =   orbcolors
-                for i in range(len(orbitals)):
+                for i in range(Norbs):
                     l = ax0.fill_between(banda['distance'],
                                          banda['energy']+multiplier*banda[orbitals[i]],
                                          banda['energy']-multiplier*banda[orbitals[i]],
                                          color=colors[i], alpha = alpha_geral, lw=0.0)
         
             #dummy plot for legend purposes
-            for i in range(len(orbitals)):
+            for i in range(Norbs):
                     l = ax0.fill_between([-1], [1],[0], color=colors[i], alpha = alpha_geral, label=orbitals[i])
             ax0.legend(loc=1, ncol=5, fontsize=12)
         
@@ -158,6 +168,8 @@ if __name__=='__main__':
                        help='The maximum value of energy in the plot (in Ha). The default is automatically chosen to show all the data available.')
     parser.add_argument('-F', '--fermi', type=float, action='store', default=0.0,
                        help='The fermi energy, in Ha. The horizontal dashed line will be placed at this value. Default is 0.0.')
+    parser.add_argument('--Eshift', type=float, action='store', default=0.0,
+                       help='A shift in energy (in Ha) to be applied to all the data. This option is useful to undo any shift in energy (such as E-Ef). Default is 0.0.')
     parser.add_argument('-T','--orbthickness', type=float, action='store', default=0.1,
                        help='This value controls the relative thickness of the bands when L-resolved. the default is 0.1.')
     parser.add_argument('-t','--linethickness', type=float, action='store', default=0.2,
@@ -172,18 +184,27 @@ if __name__=='__main__':
     parser.add_argument('-c','--linecolor', default = 'k',
                        help='The color of the bandstructure lines. This is the color of the main lines in the plot. If the -N option is used, this is the only color shown. Default is black')
     
+    parser.add_argument('-v','--verbose', action = 'store_true',
+                       help = 'Verbose. Prints some information about the found bandstructures.')
+    
     
     #get the passed arguments
     args = parser.parse_args()
-    
     if args.output == None:
         args.output=args.xml_file.rstrip('.'+args.xml_file.split('.')[-1])+'.pdf'
 
+    if args.verbose:
+        print(f'Verbose is active.\nOutput file will be {args.output}')
     dictionary_species, vertice_coords, vertice_labels = read_bands_Ldiff(args.xml_file)
+    if args.verbose:
+        print('Done reading XML file. Plotting data...')
+        print(f'   Reciprocal space path is ',' -> '.join(vertice_labels))
+        print(f'   Fermi energy will be at {args.fermi} Ha')
+        print(f'   Energy plotted will be from {args.Emin} Ha to {args.Emax} Ha')
     plot_bandstructure_atoms(dictionary_species, vertice_coords, vertice_labels,
                              l_resolved = args.Noorbital,
                              output_file = args.output,
-                             elims = (args.Emax, args.Emin),
+                             elims = (args.Emin, args.Emax),
                              efermi = args.fermi,
                              orbthickness = args.orbthickness,
                              orbalpha = args.orbalpha, 
